4a329f72cf67bbfcd29ac1bc164237cefab468b5
mandareis <amanda@depaula.xyz>
2020-10-28 20:51:06 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,4 @@
 class StudentsController < ApplicationController
-  
   def index
     @students = Student.all
   end
@@ -12,6 +11,10 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new
+    @student.first_name = (params[:first_name])
+    @student.last_name = (params[:last_name])
+    @student.save
+    redirect_to student_path(@student)
   end
-
 end
